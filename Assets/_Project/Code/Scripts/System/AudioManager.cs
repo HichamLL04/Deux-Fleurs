158725c95e20e8ac6f0b59c87bca05cedef1d01c
HichamLL04 <hichamlaaouni52@gmail.com>
2026-03-23 20:08:00 +0100

    feat: audioManager
    basic audioManager giving a temp "loop" theme between scenes.

--- Assets/_Project/Code/Scripts/System/AudioManager.cs
+++ Assets/_Project/Code/Scripts/System/AudioManager.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    AudioSource musicSource;
+    [SerializeField] AudioClip loopClip;
     void Start()
     {
-        
+        musicSource = GetComponent<AudioSource>();
+        PLayAudio(loopClip);
     }
 
     void Update()
     {
         
+    }
+
+    public void PlayOnce(AudioClip audioClip)
+    {
+        musicSource.PlayOneShot(audioClip);
+    }
+
+    public void PLayAudio(AudioClip audioClip)
+    {
+        musicSource.clip = audioClip;
+        musicSource.Play();
     }
 }
